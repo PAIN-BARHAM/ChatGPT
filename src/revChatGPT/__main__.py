5ed7df073d827c8f4ed1c439700c24eb53a98aa4
Pain <mohammedbrham98@gmail.com>
2023-01-09 11:36:21 +0400

    Update __main__.py

--- src/revChatGPT/__main__.py
+++ src/revChatGPT/__main__.py
@@ -28,6 +28,7 @@ def configure():
         config_files.append(f"{xdg_config_home}/revChatGPT/config.json")
     user_home = getenv("HOME")
     if user_home:
+        print(f"{user_home}")
         config_files.append(f"{user_home}/.config/revChatGPT/config.json")
 
     config_file = next((f for f in config_files if exists(f)), None)
@@ -37,78 +38,28 @@ def configure():
     else:
         print("No config file found.")
         raise Exception("No config file found.")
-    return config
+
+    class_to_rasa = chatGPTClass(config)
+    return class_to_rasa
 
 
-def chatGPT_main(config):
-    print("Logging in...")
-    chatbot = Chatbot(config)
-    while True:
-        prompt = get_input("\nYou:\n")
-        if prompt.startswith("!"):
-            if prompt == "!help":
-                print(
-                    """
-                !help - Show this message
-                !reset - Forget the current conversation
-                !refresh - Refresh the session authentication
-                !config - Show the current configuration
-                !rollback x - Rollback the conversation (x being the number of messages to rollback)
-                !exit - Exit the program
-                """,
-                )
-                continue
-            elif prompt == "!reset":
-                chatbot.reset_chat()
-                print("Chat session reset.")
-                continue
-            elif prompt == "!refresh":
-                chatbot.refresh_session()
-                print("Session refreshed.\n")
-                continue
-            elif prompt == "!config":
-                print(json.dumps(chatbot.config, indent=4))
-                continue
-            elif prompt.startswith("!rollback"):
-                # Default to 1 rollback if no number is specified
-                try:
-                    rollback = int(prompt.split(" ")[1])
-                except IndexError:
-                    rollback = 1
-                chatbot.rollback_conversation(rollback)
-                print(f"Rolled back {rollback} messages.")
-                continue
-            elif prompt.startswith("!setconversation"):
-                try:
-                    chatbot.config["conversation"] = prompt.split(" ")[1]
-                    print("Conversation has been changed")
-                except IndexError:
-                    print("Please include conversation UUID in command")
-                continue
-            elif prompt == "!exit":
-                break
+class chatGPTClass:
+    def __init__(self, config):
+        print("Logging in...")
+        self.chatbot = Chatbot(config)
+
+    async def get_responce_to_rasa(self, input_message="Hello"):
+        print("Start of get_responce_to_rasa")
+        prompt = input_message
+
         try:
             print("Chatbot: ")
-            message = chatbot.ask(
-                prompt, conversation_id=chatbot.config.get("conversation"), parent_id=chatbot.config.get("parent_id"))
-            print(message["message"])
+            message = self.chatbot.ask(
+                prompt,
+                conversation_id=self.chatbot.config.get("conversation"),
+                parent_id=self.chatbot.config.get("parent_id"),
+            )
+            return message["message"]
         except Exception as exc:
             print("Something went wrong!")
             print(exc)
-            continue
-
-
-def main():
-    print(
-        """
-        ChatGPT - A command-line interface to OpenAI's ChatGPT (https://chat.openai.com/chat)
-        Repo: github.com/acheong08/ChatGPT
-        """,
-    )
-    print("Type '!help' to show commands")
-    print("Press enter twice to submit your question.\n")
-    chatGPT_main(configure())
-
-
-if __name__ == "__main__":
-    main()
